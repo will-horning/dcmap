7dec3598d6609084f0b154ca4d5fae4097fb6b3d
Will <will.horning@gmail.com>
2014-07-20 15:44:32 -0400

    testing without stream

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var twitter_stream = require('./twitter_stream.js').createTwitterStream();
+// var twitter_stream = require('./twitter_stream.js').createTwitterStream();
 var classifyPoint = require('robust-point-in-polygon');
 var config = require('./config');
 
-twitter_stream.on('tweet', function(tweet){
-    if(tweet['coordinates']){
-    	var lonlat = tweet['coordinates']['coordinates'];
-    	if(classifyPoint(config.dc_bounding_polygon, lonlat) < 1){
-        	io.emit('tweet', tweet);
-    	}
-    }
-});
+// twitter_stream.on('tweet', function(tweet){
+//     if(tweet['coordinates']){
+//     	var lonlat = tweet['coordinates']['coordinates'];
+//     	if(classifyPoint(config.dc_bounding_polygon, lonlat) < 1){
+//         	io.emit('tweet', tweet);
+//     	}
+//     }
+// });
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
