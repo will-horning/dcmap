3d88f44aa69b59b8bd9c77c5b5af8663ce98bee9
Will <will.horning@gmail.com>
2014-07-20 16:32:35 -0400

    instagram challenge respone

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var twitter_stream = require('./twitter_stream.js').createTwitterStream();
+// var twitter_stream = require('./twitter_stream.js').createTwitterStream();
 var classifyPoint = require('robust-point-in-polygon');
 var config = require('./config');
 var Instagram = require('instagram-node-lib');
@@ -16,14 +16,14 @@ Instagram.media.subscribe({lat: 38.99537317916349, lng: -77.0409607887268, radiu
 //     console.log(res);
 // })
 
-twitter_stream.on('tweet', function(tweet){
-    if(tweet['coordinates']){
-    	var lonlat = tweet['coordinates']['coordinates'];
-    	if(classifyPoint(config.dc_bounding_polygon, lonlat) < 1){
-        	io.emit('tweet', tweet);
-    	}
-    }
-});
+// twitter_stream.on('tweet', function(tweet){
+//     if(tweet['coordinates']){
+//     	var lonlat = tweet['coordinates']['coordinates'];
+//     	if(classifyPoint(config.dc_bounding_polygon, lonlat) < 1){
+//         	io.emit('tweet', tweet);
+//     	}
+//     }
+// });
 
 app.set('view engine', 'jade');
 app.use(express.static('public'));
@@ -34,8 +34,8 @@ app.get('/', function(req, res){
 
 app.get('/callback', function(req, res){
     console.log('----------------------------------');
-    console.log(req.query.hub.challenge);
-    res.send(req.query.hub.challenge);
+    console.log(req.query['hub.challenge']);
+    res.send(req.query['hub.challenge']);
     // io.emit('ig callback', res);
     // var handshake =  Instagram.subscriptions.handshake(req, res);
 })
